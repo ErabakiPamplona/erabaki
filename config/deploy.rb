2fc8c6de9665be45349e3100b24ca2150cea72cc
Hilfe <daniel.hilfe@gmail.com>
2017-04-05 11:47:59 +0200

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"+
+      execute "mkdir #{shared_path}/tmp/sockets -p"
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
