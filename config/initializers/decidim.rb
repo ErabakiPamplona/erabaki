3ca433d20d0bc625363cd313dc03579df79a9c30
Hilfe <daniel.hilfe@gmail.com>
2017-04-06 14:47:44 +0200

    Set geocoder configuration

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -4,16 +4,16 @@ Decidim.configure do |config|
   config.application_name = "Erabaki Pamplona"
   config.mailer_sender    = "erabaki@pamplona.es"
   config.authorization_handlers = [ExampleAuthorizationHandler]
-  config.maximum_attachment_size = 150.megabytes 
+  config.maximum_attachment_size = 150.megabytes
   # Uncomment this lines to set your preferred locales
   # config.available_locales = %i{en ca es}
 
   # Geocoder configuration
-  # config.geocoder = {
-  #   static_map_url: "https://image.maps.cit.api.here.com/mia/1.6/mapview",
-  #   here_app_id: Rails.application.secrets.geocoder["here_app_id"],
-  #   here_app_code: Rails.application.secrets.geocoder["here_app_code"]
-  # }
+   config.geocoder = {
+     static_map_url: "https://image.maps.cit.api.here.com/mia/1.6/mapview",
+     here_app_id: Rails.application.secrets.geocoder["here_app_id"],
+     here_app_code: Rails.application.secrets.geocoder["here_app_code"]
+   }
 
   # Currency unit
   # config.currency_unit = "€"
