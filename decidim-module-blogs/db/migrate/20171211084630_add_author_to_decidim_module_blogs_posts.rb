e06a05694dfe27f019d3749e26509f08f19179c2
Hilfe <daniel.hilfe@gmail.com>
2018-05-08 16:25:56 +0200

    Archieve decidim-module-blogs (dev)

--- decidim-module-blogs/db/migrate/20171211084630_add_author_to_decidim_module_blogs_posts.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddAuthorToDecidimModuleBlogsPosts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :decidim_module_blogs_posts, :decidim_author_id, :integer, index: true
-  end
-end
