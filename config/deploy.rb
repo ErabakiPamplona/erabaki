603e6d59ddb9657e17dbf25f09625d80f09519c6
Hilfe <daniel.hilfe@gmail.com>
2018-05-08 12:23:19 +0200

    Cap version 3.10

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.9.1"
+lock "3.10.2"
 
 def deploysecret(key)
   @deploy_secrets_yml ||= YAML.load_file('config/deploy-secrets.yml')[fetch(:stage).to_s]
