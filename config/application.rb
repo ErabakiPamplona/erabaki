88a2dd0ae07fec466b214ff9e79b5ea131097cfc
Hilfe <daniel.hilfe@gmail.com>
2019-09-04 17:51:07 +0200

    Fix bug in default locale

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Erabaki
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = "Europe/Madrid"
+
+    # Locales
+    config.i18n.available_locales = %w(es eu)
+    config.i18n.default_locale = :es
   end
 end
