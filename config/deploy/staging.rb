4b0cb065f039a64cc31e4d22d86ec801aa470b74
Hilfe <daniel.hilfe@gmail.com>
2025-03-11 22:39:05 +0100

    Fix deploy staging npm path

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,5 +5,7 @@ set :branch, ENV['branch'] || :staging
 set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :staging
 set :rails_env, :production
+set :default_env, { PATH: "#{deploysecret(:npm_path)}:/usr/local/bin:/usr/bin:/bin" }
+
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w(web app db importer cron)
