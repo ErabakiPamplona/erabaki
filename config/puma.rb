f1fa97f9ae381d5d05707a0d9fe8c9961a5e616d
Hilfe <daniel.hilfe@gmail.com>
2026-01-19 20:37:13 +0100

    Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -7,9 +7,13 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
+# Prefer an explicit bind (e.g. unix socket) when provided, otherwise fall back to PORT.
 #
-port        ENV.fetch("PORT") { 3000 }
+if ENV["PUMA_BIND"]
+  bind ENV["PUMA_BIND"]
+else
+  port ENV.fetch("PORT") { 3000 }
+end
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -42,6 +46,11 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # on_worker_boot do
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 # end
+
+# Store state/pid in shared locations when provided by the deploy environment.
+#
+state_path ENV["PUMA_STATE"] if ENV["PUMA_STATE"]
+pidfile ENV["PUMA_PIDFILE"] if ENV["PUMA_PIDFILE"]
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
