d03f2d164e3696cf12c155c0b25fc45dcc266d4c
Hilfe <daniel.hilfe@gmail.com>
2026-01-19 17:26:58 +0100

    Deploy config on staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -59,3 +59,11 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+set :deploy_to, deploysecret(:deploy_to)
+set :branch, ENV['branch'] || :master
+set :ssh_options, port: deploysecret(:ssh_port)
+set :stage, :staging
+set :rails_env, :production
+set :default_env, { path: "#{deploysecret(:npm_path)}:$PATH" }
+
+server deploysecret(:server), user: deploysecret(:user), roles: %w(web app db importer cron)
